51ca182f89419b7850edd3a70787d4242d031b37
Ryan Christian <rchristian@ryanchristian.dev>
2024-01-16 00:50:08 -0600

    chore: Get rid of TW expansion plugin, unused

--- sovereign.config.js
+++ sovereign.config.js
@@ -1,11 +1,4 @@
 import { defineConfig } from '@sovereignjs/core';
-import groupingPlugin from 'vite-plugin-tailwind-grouping';
 
 export default defineConfig({
-    plugins: [
-        {
-            ...groupingPlugin(),
-            apply: 'build',
-        },
-    ],
 });
