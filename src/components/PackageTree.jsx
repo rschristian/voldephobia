5919d1e29b91beec255b450250056e8322441e7f
Ryan Christian <rchristian@ryanchristian.dev>
2024-11-19 13:48:24 -0600

    chore: Backport collapsing subtrees from deptree

--- src/components/PackageTree.jsx
+++ src/components/PackageTree.jsx
@@ -1,3 +1,5 @@
+import { useRef } from 'preact/hooks';
+
 /**
  * @param {Object} props
  * @param {import('../pkg/types.d.ts').ModuleTree} props.pkg
@@ -6,20 +8,40 @@
  * @param {string} [props.prefix='']
  */
 export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
-    let lineSymbol = prefix;
-    let childPrefix = prefix;
+    const ref = useRef(null);
+
+    let lineSymbol = prefix,
+        childPrefix = prefix;
     if (depth > 0) {
         lineSymbol += (isLast ? '└' : '├').padEnd(6, '-');
         childPrefix += (isLast ? ' ' : '│').padEnd(8, ' ');
+    } else {
+        childPrefix = ' ';
     }
 
     const decoration = pkg.poisoned
         ? 'underline(& offset-4) decoration(2 red)'
         : '';
 
+    const hasChildren = pkg.dependencies?.length > 0;
+
+    // Avoid component rerenders as the tree could be quite large
+    const toggleCollapse = () => {
+        if (ref.current) {
+            ref.current.classList.toggle('collapse-tree');
+            ref.current.firstChild.textContent = ref.current.firstChild.textContent === '+' ? '-' : '+';
+            ref.current.firstChild.classList.toggle('text-primary');
+        }
+    };
+
     return (
-        <div class={depth == 0 && 'mb-4 last:mb-2' || depth == 1 && 'ml-4'}>
-            <pre class="w-max">
+        <div class={depth == 0 ? 'mb-4 last:mb-2' : ''}>
+            <pre class="w-full text-left" ref={ref}>
+                <button
+                    class={`inline-flex items-center justify-center h-5 w-5 mr-2 bg-highlight(& dark:dark) font-bold hocus:(outline(1 & primary)) rounded ${hasChildren ? '' : 'invisible'}`}
+                    onClick={toggleCollapse}>
+                    -
+                </button>
                 {lineSymbol}
                 <a
                     class={`px-1 py-0.5 rounded bg-highlight(& dark:dark) hocus:opacity-80 ${decoration}`}
@@ -30,7 +52,7 @@ export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
                     {`${pkg.name}@${pkg.version}`}
                 </a>
             </pre>
-            {pkg.dependencies?.length &&
+            {hasChildren &&
                 pkg.dependencies.map((dep, i) => (
                     <PackageTree
                         pkg={dep}
